b1dd63181e9a8eefb8ff71b07c8a443f6dd74db4
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-12-07 10:29:49 +0530

    center align the masonry

--- src/gallery.js
+++ src/gallery.js
@@ -1,4 +1,4 @@
-import { Box, Image, Skeleton, Spinner, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Spinner, Text } from "@chakra-ui/react";
 import React, { Component } from "react";
 import { getPictures } from "./services/api";
 import Masonry from "react-responsive-masonry";
@@ -52,30 +52,34 @@ export class gallery extends Component {
           />
         }
       >
-        <Masonry columnsCount={3} gutter={5}>
-            {pictures.map((image, i) => (
-              <Box position="relative" key={image.id}>
-                {image.url_l && (
-                  <Image src={image.url_l} alt="Picture Not available" />
-                )}
-                <Box
-                  px={2}
-                  opacity="0.8"
-                  position="absolute"
-                  bottom="0"
-                  color="white"
-                  w="100%"
-                >
-                  <Text fontSize="14px" fontWeight="600">
-                    {image.title}
-                  </Text>
-                  <Text as="i" fontSize="10px">
-                    By {image.ownername}
-                  </Text>
+        <Flex justify="center">
+          <Box w="80%">
+            <Masonry columnsCount={3} gutter={5}>
+              {pictures.map((image, i) => (
+                <Box position="relative" key={image.id}>
+                  {image.url_l && (
+                    <Image src={image.url_l} alt="Picture Not available" />
+                  )}
+                  <Box
+                    px={2}
+                    opacity="0.8"
+                    position="absolute"
+                    bottom="0"
+                    color="white"
+                    w="100%"
+                  >
+                    <Text fontSize="14px" fontWeight="600">
+                      {image.title}
+                    </Text>
+                    <Text as="i" fontSize="10px">
+                      By {image.ownername}
+                    </Text>
+                  </Box>
                 </Box>
-              </Box>
-            ))}
-        </Masonry>
+              ))}
+            </Masonry>
+          </Box>
+        </Flex>
       </InfiniteScroll>
     );
   }
